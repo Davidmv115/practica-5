b662ae992a0eba3f169005565e95cda20e163e85
Davidmv115 <124394555+Davidmv115@users.noreply.github.com>
2023-05-28 21:11:44 -0700

    Add files via upload

---
+++ misfunciones.c
@@ -0,0 +1,13 @@
+#include "misfunciones.h"
+
+int sumar_enteros(int a, int b) {
+    int suma;
+    suma= a + b;
+    return suma;
+}
+
+int restar_enteros(int a, int b) {
+    int resta;
+    resta= a - b;
+    return resta;
+}
---
+++ misfunciones.h
@@ -0,0 +1,4 @@
+#define MISFUNCIONES_H
+
+int sumar_enteros(int a, int b);
+int restar_enteros(int a, int b);
